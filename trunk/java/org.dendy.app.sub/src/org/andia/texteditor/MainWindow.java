80e2ef6ff743160dd46404419eabb4029a07356c
dendy.way@gmail.com <dendy.way@gmail.com@f4ffccde-388a-575a-2cd9-47a10c9617cc>
2011-02-01 16:39:07 +0000

    + added program comment in MainWindow.java

--- trunk/java/org.dendy.app.sub/src/org/andia/texteditor/MainWindow.java
+++ trunk/java/org.dendy.app.sub/src/org/andia/texteditor/MainWindow.java
@@ -1,5 +1,12 @@
 package org.andia.texteditor;
 
+/*
+ * Program ini untuk text editor yang merefrensi ke GEdit
+ * @author : Aryandie (dendy)
+ * @since 1 Febuary 2011
+ * @license : GPL
+ * @libary swt.jar for linux
+ */
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
