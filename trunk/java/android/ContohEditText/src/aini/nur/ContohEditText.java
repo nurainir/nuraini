e3eb12e45d06e14c2853d5282229bb28c7208071
nurainir@gmail.com <nurainir@gmail.com@f4ffccde-388a-575a-2cd9-47a10c9617cc>
2011-03-18 21:40:24 +0000

    mofified ContohEditText

--- trunk/java/android/ContohEditText/src/aini/nur/ContohEditText.java
+++ trunk/java/android/ContohEditText/src/aini/nur/ContohEditText.java
@@ -1,19 +1,20 @@
 package aini.nur;
 
 /**
- * ContohEditText contoh penggunaan EditText di android
+ * ContohEditText contoh penggunaan EditText dan passing data antar activity di android
  * @author	: Nur Aini Rakhmawati
  * @since	: March 10, 2011
  * @license	: GPL
  */
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
 
 public class ContohEditText extends Activity {
     /** Called when the activity is first created. */
@@ -28,9 +29,15 @@ public class ContohEditText extends Activity {
 
 			@Override
 			public void onClick(View v) {
-				// tampilkan nilai edittext pada toast
-				 Toast.makeText(ContohEditText.this, isi.getText(),
-		                    Toast.LENGTH_SHORT).show();
+			
+				Intent i = new Intent(ContohEditText.this,Terima.class);    
+				Bundle b = new Bundle();
+				b.putString("nilai", isi.getText().toString());
+				b.putInt("x", 3);
+				i.putExtras(b);
+				startActivityForResult(i, 0);
+				
+
 
 				
 			}
--- trunk/java/android/GantiTab/gen/aini/nur/R.java
+++ trunk/java/android/GantiTab/gen/aini/nur/R.java
@@ -14,9 +14,10 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
+        public static final int Button01=0x7f050002;
         public static final int LinearLayout01=0x7f050000;
         public static final int RatingBar01=0x7f050001;
-        public static final int WebView01=0x7f050002;
+        public static final int WebView01=0x7f050003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
